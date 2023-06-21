256d25681223741cf58edf3caf32bad8c8339389
Tarun Mittal <tm6622@g.rit.edu>
2023-06-21 17:47:46 -0400

    added styles to header

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -58,9 +58,13 @@ function App() {
 }
 
 function Header() {
-  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
 
-  return <h1 style={style}>Fast React Pizza Company</h1>;
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
