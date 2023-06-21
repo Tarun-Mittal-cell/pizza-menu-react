0c7e8c3daf3ced3b17f6b5dec18fcedd91f1ea5f
Tarun Mittal <tm6622@g.rit.edu>
2023-06-21 19:38:06 -0400

    rendered pizzas

--- src/index.js
+++ src/index.js
@@ -71,20 +71,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, 
-        and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
 
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mushrooms"
-        price={12}
-        photoName="pizzas/funghi.jpg"
-      />
+      <div>
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObject={pizza} />
+        ))}
+      </div>
     </main>
   );
 }
@@ -92,11 +84,11 @@ function Menu() {
 function Pizza(props) {
   return (
     <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+      <img src={props.pizzaObject.photoName} alt={props.pizzaObject.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObject.name}</h3>
+        <p>{props.pizzaObject.ingredients}</p>
+        <span>{props.pizzaObject.price + 3}</span>
       </div>
     </div>
   );
